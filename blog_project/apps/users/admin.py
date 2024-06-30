52c646d64aaa7a10cef2aed37903febb4fc56ad1
your_github_username <your_github_email>
2024-06-30 19:03:27 +0400

    profile fixed

--- blog_project/apps/users/admin.py
+++ blog_project/apps/users/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser, Profile
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
 class CustomUserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    inlines = [ProfileInline]
+
     list_display = ('email', 'name', 'surname', 'is_active', 'is_staff', 'created_at')
     list_filter = ('is_staff', 'is_active')
     search_fields = ('email', 'name', 'surname')
     ordering = ('email',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('name', 'surname')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'created_at')}),
+        ('Personal info', {'fields': ('name', 'surname')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined', 'created_at')}),
     )
     add_fieldsets = (
         (None, {
@@ -23,28 +35,30 @@ class CustomUserAdmin(BaseUserAdmin):
     )
     readonly_fields = ('created_at', 'last_login', 'date_joined')
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('email',)  # Prevent editing email after user creation
+        return self.readonly_fields
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'profile_image', 'bio')
     search_fields = ('user__email', 'user__name', 'user__surname')
     list_filter = ('user__is_active',)
-    readonly_fields = ('user',)
-
-    fieldsets = (
-        (None, {
-            'fields': ('user',)
-        }),
-        (_('Profile Details'), {
-            'fields': ('profile_image', 'bio')
-        }),
-    )
+    readonly_fields = ('user', 'profile_image', 'bio')
 
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
+    def has_change_permission(self, request, obj=None):
         return False
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile, ProfileAdmin)
